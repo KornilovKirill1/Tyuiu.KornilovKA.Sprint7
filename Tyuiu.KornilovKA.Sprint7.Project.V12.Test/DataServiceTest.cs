30e79f27a21307655804e971f1c5669a2f024bf2
kilianAid <kilian_bs@mail.ru>
2023-12-11 21:00:45 +0500

    first commit

---
+++ Tyuiu.KornilovKA.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.KornilovKA.Sprint7.Project.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
