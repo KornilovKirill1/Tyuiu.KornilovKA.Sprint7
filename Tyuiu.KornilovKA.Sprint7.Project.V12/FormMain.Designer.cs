cc2de0ad77fe254717f8a174a4c15524c600f501
kilianAid <kilian_bs@mail.ru>
2023-12-13 22:44:18 +0500

    add: new elements in main from

--- Tyuiu.KornilovKA.Sprint7.Project.V12/FormMain.Designer.cs
+++ Tyuiu.KornilovKA.Sprint7.Project.V12/FormMain.Designer.cs
@@ -31,11 +31,24 @@ namespace Tyuiu.KornilovKA.Sprint7.Project.V12
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.toolStripUpperPanel_KKA = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonInfoProgram_KKA = new System.Windows.Forms.ToolStripButton();
             this.panelMenu_KKA = new System.Windows.Forms.Panel();
             this.panelDataGrid_KKA = new System.Windows.Forms.Panel();
-            this.toolStripButtonInfoProgram_KKA = new System.Windows.Forms.ToolStripButton();
             this.dataGridViewTable_KKA = new System.Windows.Forms.DataGridView();
+            this.toolStripDropDownButtonFile_KKA = new System.Windows.Forms.ToolStripDropDownButton();
+            this.ToolStripMenuItemOpen_KKA = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemSave_KKA = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonChangedMainonPC_KKA = new System.Windows.Forms.Button();
+            this.buttonChangedMainOnManufacturer_KKA = new System.Windows.Forms.Button();
+            this.buttonManual_KKA = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.toolStripUpperPanel_KKA.SuspendLayout();
+            this.panelMenu_KKA.SuspendLayout();
             this.panelDataGrid_KKA.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable_KKA)).BeginInit();
             this.SuspendLayout();
@@ -43,29 +56,13 @@ namespace Tyuiu.KornilovKA.Sprint7.Project.V12
             // toolStripUpperPanel_KKA
             // 
             this.toolStripUpperPanel_KKA.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButtonInfoProgram_KKA});
+            this.toolStripButtonInfoProgram_KKA,
+            this.toolStripDropDownButtonFile_KKA});
             this.toolStripUpperPanel_KKA.Location = new System.Drawing.Point(0, 0);
             this.toolStripUpperPanel_KKA.Name = "toolStripUpperPanel_KKA";
             this.toolStripUpperPanel_KKA.Size = new System.Drawing.Size(800, 25);
             this.toolStripUpperPanel_KKA.TabIndex = 0;
             this.toolStripUpperPanel_KKA.Text = "toolStrip1";
-            // 
-            // panelMenu_KKA
-            // 
-            this.panelMenu_KKA.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelMenu_KKA.Location = new System.Drawing.Point(0, 25);
-            this.panelMenu_KKA.Name = "panelMenu_KKA";
-            this.panelMenu_KKA.Size = new System.Drawing.Size(200, 425);
-            this.panelMenu_KKA.TabIndex = 1;
-            // 
-            // panelDataGrid_KKA
-            // 
-            this.panelDataGrid_KKA.Controls.Add(this.dataGridViewTable_KKA);
-            this.panelDataGrid_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelDataGrid_KKA.Location = new System.Drawing.Point(200, 25);
-            this.panelDataGrid_KKA.Name = "panelDataGrid_KKA";
-            this.panelDataGrid_KKA.Size = new System.Drawing.Size(600, 425);
-            this.panelDataGrid_KKA.TabIndex = 2;
             // 
             // toolStripButtonInfoProgram_KKA
             // 
@@ -78,14 +75,138 @@ namespace Tyuiu.KornilovKA.Sprint7.Project.V12
             this.toolStripButtonInfoProgram_KKA.Text = "О программе";
             this.toolStripButtonInfoProgram_KKA.Click += new System.EventHandler(this.toolStripButtonInfoProgram_KKA_Click);
             // 
+            // panelMenu_KKA
+            // 
+            this.panelMenu_KKA.Controls.Add(this.buttonManual_KKA);
+            this.panelMenu_KKA.Controls.Add(this.buttonChangedMainOnManufacturer_KKA);
+            this.panelMenu_KKA.Controls.Add(this.buttonChangedMainonPC_KKA);
+            this.panelMenu_KKA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu_KKA.Location = new System.Drawing.Point(0, 25);
+            this.panelMenu_KKA.Name = "panelMenu_KKA";
+            this.panelMenu_KKA.Size = new System.Drawing.Size(200, 425);
+            this.panelMenu_KKA.TabIndex = 1;
+            // 
+            // panelDataGrid_KKA
+            // 
+            this.panelDataGrid_KKA.Controls.Add(this.button2);
+            this.panelDataGrid_KKA.Controls.Add(this.button1);
+            this.panelDataGrid_KKA.Controls.Add(this.textBox4);
+            this.panelDataGrid_KKA.Controls.Add(this.textBox3);
+            this.panelDataGrid_KKA.Controls.Add(this.textBox2);
+            this.panelDataGrid_KKA.Controls.Add(this.textBox1);
+            this.panelDataGrid_KKA.Controls.Add(this.dataGridViewTable_KKA);
+            this.panelDataGrid_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDataGrid_KKA.Location = new System.Drawing.Point(200, 25);
+            this.panelDataGrid_KKA.Name = "panelDataGrid_KKA";
+            this.panelDataGrid_KKA.Size = new System.Drawing.Size(600, 425);
+            this.panelDataGrid_KKA.TabIndex = 2;
+            // 
             // dataGridViewTable_KKA
             // 
             this.dataGridViewTable_KKA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewTable_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridViewTable_KKA.Location = new System.Drawing.Point(0, 0);
             this.dataGridViewTable_KKA.Name = "dataGridViewTable_KKA";
-            this.dataGridViewTable_KKA.Size = new System.Drawing.Size(600, 425);
+            this.dataGridViewTable_KKA.Size = new System.Drawing.Size(600, 337);
             this.dataGridViewTable_KKA.TabIndex = 0;
+            // 
+            // toolStripDropDownButtonFile_KKA
+            // 
+            this.toolStripDropDownButtonFile_KKA.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButtonFile_KKA.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemOpen_KKA,
+            this.ToolStripMenuItemSave_KKA});
+            this.toolStripDropDownButtonFile_KKA.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButtonFile_KKA.Image")));
+            this.toolStripDropDownButtonFile_KKA.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButtonFile_KKA.Name = "toolStripDropDownButtonFile_KKA";
+            this.toolStripDropDownButtonFile_KKA.Size = new System.Drawing.Size(49, 22);
+            this.toolStripDropDownButtonFile_KKA.Text = "Файл";
+            // 
+            // ToolStripMenuItemOpen_KKA
+            // 
+            this.ToolStripMenuItemOpen_KKA.Name = "ToolStripMenuItemOpen_KKA";
+            this.ToolStripMenuItemOpen_KKA.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItemOpen_KKA.Text = "Открыть";
+            // 
+            // ToolStripMenuItemSave_KKA
+            // 
+            this.ToolStripMenuItemSave_KKA.Name = "ToolStripMenuItemSave_KKA";
+            this.ToolStripMenuItemSave_KKA.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItemSave_KKA.Text = "Сохранить";
+            // 
+            // buttonChangedMainonPC_KKA
+            // 
+            this.buttonChangedMainonPC_KKA.Location = new System.Drawing.Point(0, 0);
+            this.buttonChangedMainonPC_KKA.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonChangedMainonPC_KKA.Name = "buttonChangedMainonPC_KKA";
+            this.buttonChangedMainonPC_KKA.Size = new System.Drawing.Size(200, 44);
+            this.buttonChangedMainonPC_KKA.TabIndex = 0;
+            this.buttonChangedMainonPC_KKA.Text = "ЭВМ";
+            this.buttonChangedMainonPC_KKA.UseVisualStyleBackColor = true;
+            // 
+            // buttonChangedMainOnManufacturer_KKA
+            // 
+            this.buttonChangedMainOnManufacturer_KKA.Location = new System.Drawing.Point(0, 42);
+            this.buttonChangedMainOnManufacturer_KKA.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonChangedMainOnManufacturer_KKA.Name = "buttonChangedMainOnManufacturer_KKA";
+            this.buttonChangedMainOnManufacturer_KKA.Size = new System.Drawing.Size(200, 44);
+            this.buttonChangedMainOnManufacturer_KKA.TabIndex = 1;
+            this.buttonChangedMainOnManufacturer_KKA.Text = "Производитель";
+            this.buttonChangedMainOnManufacturer_KKA.UseVisualStyleBackColor = true;
+            // 
+            // buttonManual_KKA
+            // 
+            this.buttonManual_KKA.Location = new System.Drawing.Point(0, 382);
+            this.buttonManual_KKA.Name = "buttonManual_KKA";
+            this.buttonManual_KKA.Size = new System.Drawing.Size(200, 44);
+            this.buttonManual_KKA.TabIndex = 2;
+            this.buttonManual_KKA.Text = "Руководство пользователя";
+            this.buttonManual_KKA.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 344);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 343);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(221, 343);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(328, 343);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
@@ -98,10 +219,12 @@ namespace Tyuiu.KornilovKA.Sprint7.Project.V12
             this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Учёт ЭВМ";
             this.toolStripUpperPanel_KKA.ResumeLayout(false);
             this.toolStripUpperPanel_KKA.PerformLayout();
+            this.panelMenu_KKA.ResumeLayout(false);
             this.panelDataGrid_KKA.ResumeLayout(false);
+            this.panelDataGrid_KKA.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable_KKA)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -115,6 +238,18 @@ namespace Tyuiu.KornilovKA.Sprint7.Project.V12
         private System.Windows.Forms.Panel panelMenu_KKA;
         private System.Windows.Forms.Panel panelDataGrid_KKA;
         private System.Windows.Forms.DataGridView dataGridViewTable_KKA;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButtonFile_KKA;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemOpen_KKA;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemSave_KKA;
+        private System.Windows.Forms.Button buttonChangedMainOnManufacturer_KKA;
+        private System.Windows.Forms.Button buttonChangedMainonPC_KKA;
+        private System.Windows.Forms.Button buttonManual_KKA;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
